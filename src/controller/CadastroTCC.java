5f52f2c6dc86baa37379d45698b641ab4fe505d1
Leandro <Leandro@LAPTOP-MA11G6EE>
2018-04-13 17:08:56 -0300

    Primeiro commit - Tela Login quase pronta, falta apenas botões;

---
+++ src/controller/CadastroTCC.java
@@ -0,0 +1,12 @@
+package controller;
+
+import view.Login;
+
+public class CadastroTCC {
+    
+    public static void main(String Args[]){
+        
+         Login login = new Login();
+    }
+    
+}
